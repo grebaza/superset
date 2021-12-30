6bb5b918083a70b36ca42573f72f617c561ce19e
Guillermo Rebaza <grebaza@gmail.com>
2021-12-30 21:36:13 +0000

    create 1.4 branch

---
+++ docker/get_version.py
@@ -0,0 +1,10 @@
+import io
+import json
+import os
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+PACKAGE_JSON = os.path.join(BASE_DIR, "superset-frontend", "package.json")
+with open(PACKAGE_JSON, "r") as package_file:
+    version_string = json.load(package_file)["version"]
+
+print(version_string)
